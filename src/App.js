a00ab825ae33de4d4945e3fedde954233794c220
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-09 15:00:13 -0400

    Add react router component Add Home component Add signUp component Add signIn component test components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
 
 export default App;
