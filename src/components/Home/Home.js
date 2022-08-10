397982a5a2fa9e1dd8b08e2d9dd9adac76810bcd
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-10 13:15:31 -0400

    home page nav component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,50 @@
+import "./home.css";
 import React from "react";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container className="container">
+      <Row>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">Add</Nav.Link>
+          </LinkContainer>
+        </Col>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">$$$$</Nav.Link>
+          </LinkContainer>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">Withdraw</Nav.Link>
+          </LinkContainer>
+        </Col>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">History</Nav.Link>
+          </LinkContainer>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">Settings</Nav.Link>
+          </LinkContainer>
+        </Col>
+        <Col>
+          <LinkContainer to="/" className="link__container">
+            <Nav.Link className="link">Top-Bets</Nav.Link>
+          </LinkContainer>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
