6610692f118a63cc1afc18d7e892ded15bde3abb
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-12 10:29:49 -0400

    added signup link to sign in component

--- src/components/Users/Display/SignIn/SignIn.js
+++ src/components/Users/Display/SignIn/SignIn.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h3>Log In</h3>
+      <Link to="/signUp">Create Account</Link>
+    </div>
+  );
 };
 
 export default SignIn;
