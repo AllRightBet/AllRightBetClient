4626d32f87e4a61dd1e705bd5b338aec7fd160f3
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-12 10:18:22 -0400

    Add check if user is signed in feature to home Add feature to navbar

--- src/components/Global/Navbar/Navbar.js
+++ src/components/Global/Navbar/Navbar.js
@@ -3,9 +3,10 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
-const Navbar = () => {
+const Navbar = ({ setUser, user }) => {
   const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-  return (
+
+  const notLoggedIn = (
     <Nav
       className="nav__container"
       variant="pills"
@@ -19,12 +20,30 @@ const Navbar = () => {
       >
         <NavDropdown.Item eventKey="4.1">Log In</NavDropdown.Item>
         <NavDropdown.Item eventKey="4.2">Create Account</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  );
+
+  const loggedIn = (
+    <Nav
+      className="nav__container"
+      variant="pills"
+      activeKey="/home"
+      onSelect={handleSelect}
+    >
+      <NavDropdown
+        className="nav_dropdown-container"
+        title="Dropdown"
+        id="nav-dropdown"
+      >
         <NavDropdown.Item eventKey="4.3">Settings</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item eventKey="4.4">Log Out</NavDropdown.Item>
       </NavDropdown>
     </Nav>
   );
+
+  return <>{user !== null ? loggedIn : notLoggedIn}</>;
 };
 
 export default Navbar;
