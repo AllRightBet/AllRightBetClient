c29a889ea877acd3534b9f799c402d03e88e76d4
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-12 07:05:04 -0400

    fix imports

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Users/Display/Home/Home";
 import SignIn from "./components/Users/Display/SignIn/SignIn";
 import SignUp from "./components/Users/Display/SignUp/SignUp";
 
+<<<<<<< HEAD
 import AdminDashboard from "./components/Admin/Display/AdminDashboard.js";
 import AdminCreateFightCard from "./components/Admin/Forms/Admin_CreateFightCard"
 import SignUpForm from "./components/Users/Forms/SignUpForm";
@@ -17,10 +18,17 @@ import DisplayModels from "./components/DisplayModels";
 
 =======
 import AdminDashboard from "./components/Admin/AdminDashboard";
+=======
+import AdminDashboard from "./components/Display/Admin/AdminDashboard";
+>>>>>>> fba35d6 (fix imports)
 import AdminCreateFightCard from "./components/Forms/Admin_CreateFightCard";
 import AdminCreateUser from "./components/Forms/Admin_CreateUser";
+import SignUpForm from "./components/Users/Forms/SignUpForm";
 import GetFightCard from "./components/GetFightCard/GetFightCard";
+<<<<<<< HEAD
 >>>>>>> e62d6ca (Add fightcard component and style)
+=======
+>>>>>>> fba35d6 (fix imports)
 
 const App = () => {
 
@@ -35,12 +43,16 @@ const App = () => {
         <Route path="/signIn" element={<SignIn />} />
         <Route path="/signUp" element={<SignUp />} />
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         <Route path="/user-bets" element={<DisplayModels url="/bet?relative_user=1" />} />
         <Route path="/user-fight-cards" element={<DisplayModels url="/fight-card?relative_user=1" />} />
 =======
         <Route path="/getFightCard" element={<GetFightCard />} />
 >>>>>>> e62d6ca (Add fightcard component and style)
+=======
+        <Route path="/getFightCard" element={<GetFightCard />} />
+>>>>>>> fba35d6 (fix imports)
 
         {/* ADMIN INTERFACE */}
         {/* todo : needs admin authentication */}
