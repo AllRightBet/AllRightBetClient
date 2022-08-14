f878ba7a8e867f0773ad655ab24852433026ae60
itMeansBigMountain <affan.fareed@gmail.com>
2022-08-14 16:10:42 -0500

    login component

--- src/components/Users/Display/SignIn/SignIn.js
+++ src/components/Users/Display/SignIn/SignIn.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Login from "../../Forms/Login/Login";
+
+
+import './SignIn.css';
 
 const SignIn = () => {
   return (
-    <div>
-      <h3>Log In</h3>
+    <div className="login-wrapper">
+      <Login/>
       <Link to="/signUp">Create Account</Link>
     </div>
   );
