f9e0a22cf40aab8942570bf458e5f2c5e3cbb15a
itMeansBigMountain <affan.fareed@gmail.com>
2022-08-15 04:59:09 -0500

    navbar links

--- src/components/Global/Navbar/Navbar.js
+++ src/components/Global/Navbar/Navbar.js
@@ -38,6 +38,11 @@ const Navbar = ({ setUser, user }) => {
         title="Dropdown"
         id="nav-dropdown"
       >
+        <NavDropdown.Item eventKey="4.3">Add Funds</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">Withdraw Funds</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">View Weekly Event</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">Event History</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">Top-Bets</NavDropdown.Item>
         <NavDropdown.Item eventKey="4.3">Settings</NavDropdown.Item>
         <NavDropdown.Divider />
         <NavDropdown.Item eventKey="4.4">Log Out</NavDropdown.Item>
